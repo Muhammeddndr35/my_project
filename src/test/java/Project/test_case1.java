c340dd6c24d7cabe1131c9a9dbb90b7a0d8a8dd9
damnb <muhammedundar0@gmail.com>
2024-06-24 18:13:58 +0300

    2

--- src/test/java/Project/test_case1.java
+++ src/test/java/Project/test_case1.java
@@ -1,4 +1,4 @@
-package video_1;
+package Project;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import javax.swing.*;
 import java.time.Duration;
 
 public class test_case1 {
@@ -31,14 +30,14 @@ public class test_case1 {
         WebElement logo= driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
         Assert.assertTrue(logo.isDisplayed());
         WebElement product= driver.findElement(By.xpath("//h2[text()='Rs. 500']"));
-        WebElement ekleme= driver.findElement(By.xpath("//a[@data-product-id='1']"));
+        WebElement addproduct= driver.findElement(By.xpath("//a[@data-product-id='1']"));
         Actions actions = new Actions(driver);
-        actions.moveToElement(product).click(ekleme).perform();
+        actions.moveToElement(product).click(addproduct).perform();
         WebElement viewproduct= driver.findElement(By.xpath("//a[text()='View Product']"));
         viewproduct.click();
-        String beklenenurl= "https://automationexercise.com/product_details/1";
-        String sonuçurl= driver.getCurrentUrl();
-        Assert.assertEquals(beklenenurl,sonuçurl);
+        String expectedurl= "https://automationexercise.com/product_details/1";
+        String actualurl= driver.getCurrentUrl();
+        Assert.assertEquals(expectedurl,actualurl);
         WebElement ordernumber= driver.findElement(By.xpath("//input[@type='number']"));
         actions.click(ordernumber)
                 .sendKeys(Keys.BACK_SPACE)
@@ -50,8 +49,8 @@ public class test_case1 {
         clickbutton.click();
         WebElement viewcart= driver.findElement(By.xpath("//a[@href='/view_cart']"));
         viewcart.click();
-        WebElement sayıkontrol= driver.findElement(By.xpath("//button[text()='5']"));
-        Assert.assertTrue(sayıkontrol.isDisplayed());
+        WebElement menstrualcontrol= driver.findElement(By.xpath("//button[text()='5']"));
+        Assert.assertTrue(menstrualcontrol.isDisplayed());
         //Assert.assertEquals("Order number is not 4", "4", ordernumber.getText());
         driver.close();
 
